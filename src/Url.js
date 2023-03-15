81bbc88400868ef8786dd87bc93436ad5e54f355
gh0osty-Akdroid <101495769+gh0osty-Akdroid@users.noreply.github.com>
2023-03-16 02:37:40 +0545

    Update 1st url.js

--- src/Url.js
+++ src/Url.js
@@ -1,40 +1,34 @@
+import { useSelector } from "react-redux";
 
 import axios from 'axios'
 
 
 
 
-export const Url = () => {
-  const site = (JSON.parse(localStorage.getItem("state")))?.site?.site
-  console.log("site", site)
+export const Url = axios.create({
+    baseURL: `http://44.232.151.192:6001/api/v1/user`,
+  
+})  
 
-  return axios.create({
-    baseURL: `http://localhost:6001/api/v1/user`,
-    headers: {
-      "site": site || null,
-      'Access-Control-Allow-Origin': ['http://44.237.37.194:6001', "http://52.26.60.188:6969/"]
-    }
-  })
-}
+    
 
-export const TokenUrl = () => {
-  const token = (JSON.parse(localStorage.getItem("state")))?.authToken?.authToken
-  const site = (JSON.parse(localStorage.getItem("state")))?.site?.site
-
-
-  return axios.create({
-    baseURL: `http://localhost:6001/api/v1/user`,
-    headers: {
-      "Authorization": `Bearer ${token}`,
-      "site": site,
-      'Access-Control-Allow-Origin': ['http://44.237.37.194:6001', "http://52.26.60.188:6969/"]
-    }
-
-  })
-}
+export const TokenUrl =()=>{
+    const token = (JSON.parse(localStorage.getItem("state")))?.authToken?.authToken
+    const site = (JSON.parse(localStorage.getItem("state")))?.site?.site
+   
+    
+    return axios.create({
+        baseURL: `http://44.232.151.192:6001/api/v1/user`,
+        headers:{
+            "Authorization":`Bearer ${token}`,
+            "site" : site,
+            'Access-Control-Allow-Origin': ['http://44.232.151.192:6001',"http://3.131.17.220:6969/"]
+        }
+      
+    })
+} 
 
 
-export const GameImgUrl = "http://52.26.60.188:6969/"
-export const ImgUrl = "http://44.237.37.194:6001/"
-export const ImgUrl2 = "http://localhost:6001/"
-
+export const  GameImgUrl ="http://3.131.17.220:6969/"
+export const  ImgUrl ="http://44.232.151.192:6001/"
+export const  ImgUrl2 ="http://localhost:6001/"

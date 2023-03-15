c13a1b5e8529d6c0b8a6cab557a724c1ebee5b8d
gh0osty-Akdroid <101495769+gh0osty-Akdroid@users.noreply.github.com>
2023-03-16 02:38:27 +0545

    Update url.js

--- src/utilities/Urls.js
+++ src/utilities/Urls.js
@@ -1,31 +1,35 @@
 import { useSelector } from "react-redux";
-/* eslint-disable */
 
 import axios from 'axios'
 
+
+
+
 export const Url = axios.create({
-    baseURL: `http://44.237.37.194:6001/api/v1/user`
-})
+    baseURL: `http://44.232.151.192:6001/api/v1/user`,
+  
+})  
 
+    
 
-
-export var TokenUrl = function TokenUrl() {
-    var token = JSON.parse(localStorage.getItem("state"))?.authToken?.authToken || null;
-    var site = JSON.parse(localStorage.getItem("state"))?.site?.site || null;
-
+export const TokenUrl =()=>{
+    const token = (JSON.parse(localStorage.getItem("state")))?.authToken?.authToken
+    const site = (JSON.parse(localStorage.getItem("state")))?.site?.site
+   
+    
     return axios.create({
-        baseURL: `http://44.237.37.194:6001/api/v1/user`,
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "site": site,
-            'Access-Control-Allow-Origin': ['http://44.237.37.194:6001', "http://52.26.60.188:6969/"]
+        baseURL: `http://44.232.151.192:6001/api/v1/user`,
+        headers:{
+            "Authorization":`Bearer ${token}`,
+            "site" : site,
+            'Access-Control-Allow-Origin': ['http://44.232.151.192:6001',"http://3.131.17.220:6969/"]
         }
-
+      
     })
-}
+} 
 
 
-export const GameImgUrl = "http://3.131.17.220:6969/"
-export const ImgUrl = "http://44.237.37.194:6001/"
-export const ImgUrl2 = "http://localhost:6001/"
+export const  GameImgUrl ="http://3.131.17.220:6969/"
+export const  ImgUrl ="http://44.232.151.192:6001/"
+export const  ImgUrl2 ="http://localhost:6001/"
 
